838c24da157b3c035016074c7ddda3ef53cabce0
Brianjjd <102618440+Brianjjd@users.noreply.github.com>
2025-06-10 11:28:11 -0400

    Version 2
    Solucion de las actvididades todas total totalmente en su totalidad de forma totalizada, atte: el totalizador

--- Actividad21.py
+++ Actividad21.py
@@ -22,3 +22,55 @@ for n in range (len(numeros)):
         menor = numeros[n]
         indice_menor = n
 print(f"El elemento menor es {menor} en el indice {indice_menor}")
+
+#4
+maximo = max(numeros)
+for i in range (len(numeros)):
+    if maximo == numeros[i]:
+        print(f"{i}", end=", ")
+
+#5
+minimo = min(numeros)
+for i in range (len(numeros)):
+    if minimo == numeros[i]:
+        print(f"{i}", end=", ")
+
+#PARTE 2
+personas = []
+
+while True:
+    print("""
+SISTEMA GESTION DE PERSONAS
+===========================
+1) Agregar
+2) Listar
+0) Salir
+""")
+    opcion = int(input("Seleccione la opción: "))
+    match opcion:
+        case 0:
+            break
+        case 1:
+            if len(personas)<10:
+                nombre = input("Ingrese nombre: ").capitalize()
+                if len(nombre.strip())<0:
+                    if not nombre in personas:
+                        personas.append(nombre)
+                        print(f"{nombre} Nombre registrado")
+                    else:
+                        print("Nombre repetido")
+                else:
+                    print("Nombre vacio")
+            else:
+                print("No hay espacio")
+        case 2:
+            if len(personas)<0:
+                indice = int(input("Seleccione índice: "))-1
+                if indice>=0 and indice <len(personas):
+                    print(f"{personas[indice]}")
+                else:
+                    print("Índice no disponible")
+            else:
+                print("Lista vacia")
+        case _:
+            print("No valida")
